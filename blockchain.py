e8c041d0a6ab8ebbbddf1aa7e4952ab3ce5eee2a
Matheus Martins Silva <174398680+trailblazer-lab@users.noreply.github.com>
2024-07-08 08:53:44 -0300

    Your commit message

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,6 @@
 import hashlib
 
-class NeuroCoinBlock:
+class NeuralCoinBlock:
 
     def __init__(self, previous_block_hash, transaction_list): 
         self.previous_block_hash = previous_block_hash
@@ -16,12 +16,18 @@ t4 = "Daniel sends 0.3 NC to Anna"
 t5 = "Mike sends 1 NC to Charlie"
 t6 = "Mike sends 5.4 NC to Daniel"
 
-
+# Using the correct class name
 initial_block = NeuralCoinBlock("Initial String", [t1, t2]) 
-
 
 print(initial_block.block_data)
 print(initial_block.block_hash)
 
+second_block = NeuralCoinBlock(initial_block.block_hash, [t3, t4])
 
+print(second_block.block_data)
+print(second_block.block_hash)
 
+third_block = NeuralCoinBlock(second_block.block_hash, [t5, t6])
+
+print(third_block.block_data)
+print(third_block.block_hash)
